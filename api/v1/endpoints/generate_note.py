a6be2c92e0bdd428656b1de13a23691cf942b82e
Sairaj Jadhav <jadhavsairaj25@gmail.com>
2026-04-16 12:20:25 +0530

    feat: note generation async function in created, added api endpoint in index.html, added routes in router.py

---
+++ api/v1/endpoints/generate_note.py
@@ -0,0 +1,92 @@
+import asyncio
+from fastapi import APIRouter, Request, File, UploadFile, Form, Depends
+from pathlib import Path
+import logging
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from core.database import get_db
+from services.yt_title_service import get_yt_title
+from services.audio_download_service import download_audio
+from services.audio_transcribe_service import transcribe_audio_whisper
+from services.generate_notes_service import generate_note_from_transcription
+
+from models.db_models import Notes
+
+router = APIRouter(prefix="/note", tags=["Note"])
+
+logger = logging.getLogger(__name__)
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
+
+# def login_required()
+
+@router.post("/generate")
+async def generate_note(
+    request: Request,
+    link: str = Form(...),
+    audio_file: UploadFile = File(...),
+    db: AsyncSession = Depends(get_db),
+    # user=Depends(login_required),
+):
+    audio_bytes = await audio_file.read()
+    logger.info(
+        "Received audio upaload: %s bytes, link=%s", len(audio_bytes)
+    )  
+    # 1. Validate Request
+    if not link:
+        return HTMLResponse(
+            "<div class='text-danger'>Youtube link is required.</div>"
+        )
+    
+    
+    # 2. Get title
+    title = get_yt_title(link)
+    if not title:
+        return HTMLResponse(
+            "<div class='text-danger'>Failed to fetch YouTube title.</div>"
+        )
+    
+    # 3. Download audio
+    file_path = download_audio(link)
+    if not file_path:
+        return HTMLResponse(
+            "<div class='text-danger'>Failed to download audio.</div>"
+        )
+    
+    loop = asyncio.get_running_loop()
+    
+    # 4. transcribe
+    transcription: str = await loop.run_in_executor(
+        None, transcribe_audio_whisper, file_path
+    )
+    if not transcription or "Failed" in transcription:
+        return HTMLResponse(
+            "<div class='text-danger'>Failed to get transcript.</div>"
+        )
+
+    
+    # 5. Generate Note
+    note_content = generate_note_from_transcription(transcription)
+    if not note_content or "error" in note_content.lower():
+        return HTMLResponse(
+            f"<div class='text-danger'>{note_content}</div>"
+        )
+    
+    # 6. Save to db
+    note = Notes(
+        youtube_link = link,
+        title = title,
+        content = note_content
+    )
+
+    db.add(note)
+    await db.commit()
+    await db.refresh(note)
+
+    return templates.TemplateResponse(request, "index.html", {"note_content": note_content})
+
+    
---
+++ services/generate_notes_service.py
@@ -0,0 +1,61 @@
+import logging 
+from google import genai
+from core.config import get_settings
+from google.genai import types
+
+logger = logging.getLogger(__name__)
+
+settings = get_settings()
+
+# Initialize client once (singleton style)
+gemini_client = genai.Client(api_key=settings.GEMINI_API_KEY)
+
+def generate_note_from_transcription(transcription: str) -> str:
+    if not transcription or len(transcription.strip()) < 20:
+        return "Error: Transcription too short or Invalid"
+    
+    try:
+         prompt = f"""
+                    You are an expert academic note generator.
+
+                    Convert the following YouTube transcript into high-quality, structured revision notes.
+
+                    ### Instructions:
+                    - Extract only meaningful insights
+                    - Remove filler, repetition, promotions
+                    - Use headings and subheadings
+                    - Bullet points only
+                    - Highlight important keywords in **bold**
+                    - Convert content into:
+                      - Definitions
+                      - Step-by-step processes
+                      - Tables (if comparison)
+                      - Flowcharts (text format if needed)
+                    - Add:
+                      - Examples section
+                      - Quick Revision Box
+                      - Actionable Steps (if practical)
+                    - Keep concise but complete
+                    - Make output clean, structured, and revision-friendly
+
+                    ### Transcript:
+                    {transcription}
+                """
+         response = gemini_client.models.generate_content(
+             model="gemini-2.5-flash",
+             contents=prompt,
+             config=types.GenerateContentConfig(
+                 system_instruction="You are a professional AI-powered academic note writer.",
+                 temperature=0.3,
+                 max_output_tokens=4096,
+             ),
+         )
+
+         if not response or not response.text:
+             return "Error: Empty response from AI"
+         
+         return response.text.strip()
+    
+    except Exception as e:
+        logger.error(f"Gemini API error: {e}", exc_info=True)
+        return f"Error generating blog: {str(e)}"
